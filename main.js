f8f3df39f797802f9fa2538a414b6f810816bc97
Shion Kou <cyg65@foxmail.com>
2018-10-16 15:26:43 +0900

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-$(document).ready(function () {
-    $('#playbtn').on('click', function () {
-        $('video').trigger('play');;
-    });
+$(document).ready(function() {
+  setTimeout(function() {
+    $("#playIcon").show();
+  }, 2700);
+  $("#overlay").animate({ opacity: "0.6" }, 2600, "swing");
+  $("#playbtn>img").fadeIn("slow");
+  $("#playbtn").one("click", function() {
+    $("#playbtn>img")
+      .fadeOut(400)
+      .fadeIn("slow");
+    setTimeout(function() {
+      $("#playbtn>img").attr("src", "./assets/landingpage_kisetsu_2018.png");
+    }, 400);
+    $("#playbtn>p").hide();
+    $("video").trigger("play");
+    $("#playbtn")
+      .delay(800)
+      .animate(
+        {
+          top: "12%"
+        },
+        {
+          duration: 2700,
+          easing: "swing"
+        }
+      );
+    $("#overlay").fadeTo("slow", 0);
+  });
 });
