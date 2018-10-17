2fbf4caed24c0ca7d4fd895b106ae4568fd6692e
Shion Kou <cyg65@foxmail.com>
2018-10-17 11:20:11 +0900

    added check video load

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-$(document).ready(function() {
-  setTimeout(function() {
-    $("#playIcon").show();
-  }, 2700);
-  $("#overlay").animate({ opacity: "0.6" }, 2600, "swing");
-  $("#playbtn>img").fadeIn("slow");
-  $("#playbtn").one("click", function() {
-    $("#playbtn>img")
-      .fadeOut(400)
-      .fadeIn("slow");
-    setTimeout(function() {
-      $("#playbtn>img").attr("src", "./assets/landingpage_kisetsu_2018.png");
-    }, 400);
-    $("#playbtn>p").hide();
-    $("video").trigger("play");
-    $("#playbtn")
-      .delay(800)
-      .animate(
-        {
-          top: "12%"
-        },
-        {
-          duration: 2700,
-          easing: "swing"
-        }
-      );
-    $("#overlay").fadeTo("slow", 0);
-  });
-});
+$(document).ready(function () {
+    var video = document.getElementById("hero-video");
+    setTimeout(function () {
+        $("#playIcon").show();
+    }, 2700);
+    $("#overlay").animate({
+        opacity: "0.6"
+    }, 2600, "swing");
+    $("#playbtn>img").fadeIn("slow");
+    video.addEventListener('loadeddata', function () {
+        $("#playbtn").one("click", function () {
+            $("#playbtn>img")
+                .fadeOut(400)
+                .fadeIn("slow");
+            setTimeout(function () {
+                $("#playbtn>img").attr("src", "./assets/landingpage_kisetsu_2018.png");
+            }, 400);
+            $("#playbtn>p").hide();
+            $("video").trigger("play");
+            $("#playbtn")
+                .delay(800)
+                .animate({
+                    top: "12%"
+                }, {
+                    duration: 2700,
+                    easing: "swing"
+                });
+            $("#overlay").fadeTo("slow", 0);
+        });
+    });
+}, false);
