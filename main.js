529e4b1c4a2f99493b2983206dc734a68c314196
Shion Kou <cyg65@foxmail.com>
2018-10-18 10:40:48 +0900

    load video & animation issues resolved

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
 $(document).ready(() => {
-    setTimeout(() => {
-        $("#playIcon").show();
-    }, 2700);
-    // $("#overlay").animate({
-    //     opacity: "0.6"
-    // }, 2600, "swing");
-    $("#playbtn>img").fadeIn("slow");
-    $("#playbtn").one("click touchstart", () => {
-        setTimeout(() => {
-            $("video").trigger("play");
-        }, 5000);
-        $("#playIcon").hide();
-        $("#loadIcon").show();
+  setInterval(() => {
+    $("#playIcon")
+      .fadeOut(1000)
+      .fadeIn(1000);
+  }, 2000);
+  // $("#overlay").animate({
+  //     opacity: "0.6"
+  // }, 2600, "swing");
+  $("#playbtn>img").fadeIn("slow");
+  $("#playbtn").one("click touchstart", () => {
+    // setTimeout(() => {
+    $("video").trigger("play");
+    // }, 5000);
+    $("#playIcon").hide();
+    $("#loadIcon").show();
+  });
+  var $video = $("#hero-video");
+  $video[0].onplay = () => {
+    $("#playbtn>img").fadeOut(400, () => {
+      $("#playbtn>img")
+        .attr("src", "./assets/landingpage_kisetsu_2018.png")
+        .fadeIn();
     });
-    var $video = $("#hero-video");
-    $video[0].onplay = () => {
-        $("#playbtn>img")
-            .fadeOut(400)
-            .fadeIn("slow");
-        setTimeout(() => {
-            $("#playbtn>img").attr("src", "./assets/landingpage_kisetsu_2018.png");
-        }, 400);
-        $("#playbtn>p").hide();
-        $("#playbtn")
-            .delay(800)
-            .animate({
-                top: "12%"
-            }, {
-                duration: 2700,
-                easing: "swing"
-            });
-        // $("#overlay").fadeTo("slow", 0);
-    };
+    $("#playbtn>p").hide();
+    $("#playbtn")
+      .delay(800)
+      .animate(
+        {
+          top: "12%"
+        },
+        {
+          duration: 2700,
+          easing: "swing"
+        }
+      );
+    // $("#overlay").fadeTo("slow", 0);
+  };
 });
