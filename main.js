f2c110bdba27793eb53e10d8a9f22d51189b1087
Shion Kou <cyg65@foxmail.com>
2018-10-23 15:01:59 +0900

    video play

--- main.js
+++ main.js
@@ -1,45 +1,46 @@
 $(document).ready(() => {
+  playAnimation = () => {
+    var $video = $("#hero-video");
+    videoElment = $video[0];
+    playAction = () => {
+      $("#playbtn>img").fadeOut(400, () => {
+        $("#playbtn>img")
+          .attr("src", "./assets/landingpage_kisetsu_2018.png")
+          .fadeIn();
+      });
+      $("#playbtn>p").hide();
+      $("#playbtn")
+        .delay(800)
+        .animate({
+          top: "12%"
+        }, {
+          duration: 2700,
+          easing: "swing"
+        });
+    };
+    $video.on('canplaythrough', playAction);
+    if (videoElment.readyState > 3) {
+      playAction()
+    }
+  }
+
   setInterval(() => {
     $("#playIcon")
       .fadeOut(1000)
       .fadeIn(1000);
   }, 2000);
-  // $("#overlay").animate({
-  //     opacity: "0.6"
-  // }, 2600, "swing");
   $("#playbtn>img").fadeIn("slow");
   $("#playbtn").one("click touchstart", () => {
-    // setTimeout(() => {
     $("video").trigger("play");
-    // }, 5000);
     $("#playIcon").hide();
     $("#loadIcon").show();
+    playAnimation()
   });
-  var $video = $("#hero-video");
-  $video[0].onplay = () => {
-    $("#playbtn>img").fadeOut(400, () => {
-      $("#playbtn>img")
-        .attr("src", "./assets/landingpage_kisetsu_2018.png")
-        .fadeIn();
-    });
-    $("#playbtn>p").hide();
-    $("#playbtn")
-      .delay(800)
-      .animate(
-        {
-          top: "12%"
-        },
-        {
-          duration: 2700,
-          easing: "swing"
-        }
-      );
-    // $("#overlay").fadeTo("slow", 0);
-  };
+
   var $lightBtn = $(".light-btn");
   disabled = false;
-  $lightBtn.each(function() {
-    $(this).on("click touchstart", function() {
+  $lightBtn.each(function () {
+    $(this).on("click touchstart", function () {
       $(this)
         .addClass("active")
         .siblings()
@@ -52,7 +53,7 @@ $(document).ready(() => {
         .removeClass("far");
     });
 
-    $(this).on("mouseover mouseout", function() {
+    $(this).on("mouseover mouseout", function () {
       if (!$(this).hasClass("active")) {
         $(this)
           .children("i")
